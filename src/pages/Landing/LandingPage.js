246b439578c8e0f2fa87e11ca5edbd5cf0be7dba
hyderashfaq81 <haiderashfaq81@gmail.com>
2023-08-07 03:35:31 +0500

    responsive landing page

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -22,16 +22,16 @@ const LandingPage = () => {
           alt='MBA Logo'
           className={`w-60 h-60 ${loaded ? 'bounceIn' : ''}`}
         />
-        <p className='text-gray1 text-xl font-semibold mt-3'>
+        <p className='text-gray1 text-xl font-semibold mt-3 text-center'>
           A safe space for serious singles who value and want real relationships
         </p>
-        <div className='flex items-center justify-center gap-5'>
-        <div className="text-white bg-[#FF7294] w-[200px] h-[40px] flex items-center justify-center rounded-[20px] mt-10 cursor-pointer" onClick={() => navigate('login')}>Get Started</div>
-        <div className="text-white bg-[#FF7294] w-[200px] h-[40px] flex items-center justify-center rounded-[20px] mt-10 cursor-pointer" onClick={() => navigate('/home')}>Home</div>
+        <div className='w-full flex flex-col md:flex-row items-center justify-center md:gap-5'>
+        <div className="text-white bg-[#FF7294] w-full md:w-[200px] h-[40px] flex items-center justify-center rounded-[20px] mt-10 cursor-pointer" onClick={() => navigate('login')}>Get Started</div>
+        <div className="text-white bg-[#FF7294] w-full md:w-[200px] h-[40px] flex items-center justify-center rounded-[20px] mt-1 md:mt-10 cursor-pointer" onClick={() => navigate('/home')}>Home</div>
       </div>
       </div>
       <div className='flex justify-center items-center w-full text-black mt-10 bg-black py-2 px-4'>
-        <Link href="#" className='text-white text-xl font-medium'>
+        <Link href="#" className='ml-4 text-white text-xs md:text-xl font-medium'>
           www.marryingblackamerica.com
         </Link>
         <img src={twitterIcon} alt='twitter' className='w-10 h-10 ml-10'/>
